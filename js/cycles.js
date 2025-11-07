a369ed4dc327ab6dd8a6c92e136782150cdf7f6c
Mariabk14 <157731218+Mariabk14@users.noreply.github.com>
2025-11-07 12:01:53 +0100

    js

--- js/cycles.js
+++ js/cycles.js
@@ -11,16 +11,36 @@ for (let i = 2; i <= 20; i += 1) {
   console.log(i); 
 }
 
-for (let i = 1; i <= 10; i += 1) {
-    i *= 7
-    console.log(i);
+let k = 1;
+while (k <= 20) {
+  if (k % 3 === 0) {
+    k += 1;
+    continue; 
+  }
+    console.log(k);
 }
+
+console.log("Таблиця множення числа 7:");
+for (let i = 1; i <= 10; i+= 1) {
+  console.log(`7 x ${i} = ${7 * i}`);
+}
+
+let n = 15;
+for (let i = 0; i < 20; i+= 1) {
+  if (i >= n) {
+    break; 
+  }
+  console.log(i);
+}
+
 
 let i = 1;
 while (i <= 20) {
   if (i % 3 === 0) {
-    i += 1;
+    i+=1;
     continue; 
   }
-    console.log(i);
+  console.log(i);
+  i+=1;
 }
+
